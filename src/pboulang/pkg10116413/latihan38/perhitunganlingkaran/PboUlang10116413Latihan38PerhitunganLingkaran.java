85620b48f8a86e938300ae8ef482f37d06dfc804
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:18:42 +0700

    PboUlang-10116413-Latihan38-PerhitunganLingkaran

---
+++ src/pboulang/pkg10116413/latihan38/perhitunganlingkaran/PboUlang10116413Latihan38PerhitunganLingkaran.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan38.perhitunganlingkaran;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : Program untuk perhitungan lingkaran
+ */
+public class PboUlang10116413Latihan38PerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
